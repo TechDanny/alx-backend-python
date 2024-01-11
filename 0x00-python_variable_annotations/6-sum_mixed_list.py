907e775925690ca8383cd99f60c5a56956b69157
root <kokidanny15@gmail.com>
2024-01-11 19:41:45 +0300

    sum of mixed list

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -8,6 +8,9 @@ from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    sum of mixed list
+    """
     total = 0
     for x in mxd_lst:
         total += x
