b50ed4b94e464a16ad3e280147691ef0bf85cbeb
root <kokidanny15@gmail.com>
2024-01-12 00:15:41 +0300

    safe first element

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Duck typing
+"""
+
+
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
