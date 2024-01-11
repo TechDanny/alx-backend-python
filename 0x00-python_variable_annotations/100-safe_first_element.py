def92420fae736868589ef24a31c8c58bec51a7a
root <kokidanny15@gmail.com>
2024-01-12 00:20:00 +0300

    safe first element

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -8,6 +8,9 @@ from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    updated the function with the right annotation
+    """
     if lst:
         return lst[0]
     else:
