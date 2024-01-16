1dfa58e1770752c148da1f8a5a918241f653eb6e
root <kokidanny15@gmail.com>
2024-01-17 00:27:55 +0300

    async comprehension

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Async Comprehensions
+"""
+
+
+import asyncio
+from typing import List
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    """
+    returns 10 random numbers
+    """
+    numbers = [number async for number in async_generator()]
+    return numbers
