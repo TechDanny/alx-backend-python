b5036fc2b6a50dae0cea3ee43e1db1ae73d7ac30
root <kokidanny15@gmail.com>
2024-01-17 00:46:09 +0300

    measure runtime

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -11,7 +11,7 @@ import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """
     measures the total runtime and returns it
     """
