3c79ec737fee82f57a876116ccb0612ed29a9e03
root <kokidanny15@gmail.com>
2024-01-17 00:30:15 +0300

    async comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -11,7 +11,7 @@ from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     returns 10 random numbers
     """
