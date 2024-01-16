b352c8944bc92897e7691644d627c8976c794076
root <kokidanny15@gmail.com>
2024-01-16 23:20:36 +0300

    async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Async Generator
+"""
+
+
+import random
+import asyncio
+
+
+async def async_generator():
+    """
+    yields a random number between 0 to 10
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.randint(0, 10)
